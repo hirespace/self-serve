3f8a72903f902b2ec86f8213babb4dfd3fae0157
Perry Harlock <perry@hirespace.com>
2015-02-09 15:38:19 +0000

    Slidey stuff and a bit of styling on the tiles

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,36 @@
-$(document).on('click', '[data-role=add-new-user]', function (e) {
-    e.preventDefault();
-    $('[data-role=new-user-type]').show();
-});
+$(document).ready(function(){
 
-$(document).on('click', '[data-role=existing-radio]', function (e) {
-    $('[data-role=new-user-inputs]').show();
-    if ($(this).attr('data-value') === "existing") {
-        $('[data-role=new-user-inputs] input').hide();
-        $('[data-role=email-input]').show();
-    }
-    else {
-        $('[data-role=new-user-inputs] input').show();
+    $(document).on('click', '[data-role=showit]', function (e) {
+        e.preventDefault();
+        var element = $(this).attr('href');
+        
+        $(".thumbnail").removeClass('active');
+
+        if ($(element).hasClass('in')) {
+            $(this).parent().parent().removeClass('active');
+            $(element + "[data-role=showme].in").removeClass('in').addClass('collapse');
+        }
+        else {
+            $("[data-role=showme]").removeClass('in').addClass('collapse');
+            $(this).parent().parent().addClass('active');
+            $(element + "[data-role=showme]").addClass('in').removeClass('collapse');
+        }
+
+        if ($(".thumbnail.active").length == 0) {
+            $(animateSection("#waddyawant",0));
+
+        }
+        else {
+            $(animateSection(element,0));
+        }
+
+    });
+
+    // Function to animate sections
+    var animateSection = function(sectionName, offset){
+        console.log(sectionName);
+        $('html,body').animate({
+          scrollTop: $(sectionName).offset().top + offset
+        }, 1000);
     }
 });

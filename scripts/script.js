3ee06bcfa92792c118304a3df4a3218588ce47c8
Perry Harlock <perry@hirespace.com>
2015-02-10 11:28:32 +0000

    Add plan details and js to show and hide

--- scripts/script.js
+++ scripts/script.js
@@ -4,32 +4,41 @@ $(document).ready(function(){
         e.preventDefault();
         var element = $(this).attr('href');
         
-        $(".thumbnail").removeClass('active');
+        removeActiveThumbnailState();
 
         if ($(element).hasClass('in')) {
-            $(this).parent().parent().removeClass('active');
+            $(element + "-thumb").removeClass('active');
             $(element + "[data-role=showme].in").removeClass('in').addClass('collapse');
         }
         else {
             $("[data-role=showme]").removeClass('in').addClass('collapse');
-            $(this).parent().parent().addClass('active');
+            $(element + "-thumb").addClass('active');
             $(element + "[data-role=showme]").addClass('in').removeClass('collapse');
         }
 
         if ($(".thumbnail.active").length == 0) {
-            $(animateSection("#waddyawant",0));
-
+            $(animateSection("#home",0));
         }
         else {
             $(animateSection(element,0));
         }
     });
 
+    $(document).on('click', '[data-role=closer]', function (e) {
+        e.preventDefault();
+        removeActiveThumbnailState();
+        $(animateSection("#home",0));
+        $("[data-role=showme].in").removeClass('in').addClass('collapse');
+    });
+
     // Function to animate sections
     var animateSection = function(sectionName, offset){
-        console.log(sectionName);
         $('html,body').animate({
           scrollTop: $(sectionName).offset().top + offset
         }, 1000);
+    }
+
+    var removeActiveThumbnailState = function(){
+        $(".thumbnail").removeClass('active');
     }
 });
